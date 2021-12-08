9ef86087aecd75039fdc014ff1e8c2bc99a62498
Dinopony <dinopony.gg@gmail.com>
2021-12-08 14:36:35 +0100

    Added a patches catalog header

---
+++ patches/patches.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <landstalker_lib/md_tools.hpp>
+#include <landstalker_lib/model/world.hpp>
+
+// Global patches
+void add_functions_to_items_on_use(md::ROM& rom, bool consumable_record_book);
+void add_statue_of_jypta_effect(md::ROM& rom);
+void alter_fahl_challenge(md::ROM& rom, const World& world);
+void alter_gold_rewards_handling(md::ROM& rom, World& world);
+void alter_lantern_handling(md::ROM& rom, const World& world);
+void alter_ui_color(md::ROM& rom, uint16_t ui_color);
+void fix_hud_tilemap(md::ROM& rom);
+void fix_item_checks(md::ROM& rom);
+void patch_game_init(md::ROM& rom, const World& world, bool add_ingame_tracker);
+void handle_additional_jewels(md::ROM& rom, World& world, uint8_t jewel_count);
+void make_sword_of_gaia_work_in_volcano(md::ROM& rom);
+void normalize_special_enemies_hp(md::ROM& rom, bool fix_tree_cutting_glitch);
+
+// alter_items_consumability.cpp patches
+void make_pawn_ticket_consumable(md::ROM& rom);
+void make_key_not_consumed_on_use(md::ROM& rom);
+
+// quality_of_life.cpp patches
+void alter_item_order_in_menu(md::ROM& rom);
+void quicken_gaia_effect(md::ROM& rom);
+
+// story_dependencies.cpp patches
+void make_massan_elder_reward_not_story_dependant(md::ROM& rom);
+void make_lumberjack_reward_not_story_dependant(md::ROM& rom);
+void change_falling_ribbon_position(md::ROM& rom);
+void make_tibor_always_open(md::ROM& rom);
+void make_gumi_boulder_push_not_story_dependant(World& world);
+void make_falling_ribbon_not_story_dependant(World& world);
