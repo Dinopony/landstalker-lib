44273afeb5d4671a403249c31fdf1abe5bbd42ca
Dinopony <dinopony.gg@gmail.com>
2021-12-09 15:33:14 +0100

    Removed "ends_with" function which is now part of C++20 standard

--- tools/tools.hpp
+++ tools/tools.hpp
@@ -93,13 +93,6 @@ namespace tools
         std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::tolower(c); });
     }
 
-    inline bool ends_with(const std::string& haystack, const std::string& needle)
-    {
-        if (needle.size() > haystack.size())
-            return false;
-        return std::equal(needle.rbegin(), needle.rend(), haystack.rbegin());
-    }
-
     inline bool dump_json_to_file(const Json& json, const std::string& file_path)
     {
         std::ofstream file(file_path);
