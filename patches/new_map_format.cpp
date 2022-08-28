c978ece6480e6aab688883f94a76e9a8e5638b5c
Dinopony <dinopony.gg@gmail.com>
2022-08-28 18:29:00 +0200

    Improved performance when clearing map before loading

--- patches/new_map_format.cpp
+++ patches/new_map_format.cpp
@@ -14,19 +14,53 @@ constexpr uint16_t LINE_SIZE_IN_BYTES = LINE_SIZE_IN_WORDS * 2;
  * @param rom
  * @return
  */
-static uint32_t inject_func_set_longs(md::ROM& rom)
+static uint32_t inject_func_clear_map_data(md::ROM& rom)
 {
-    md::Code func_set_longs;
+    md::Code func;
     {
-        func_set_longs.label("loop");
-        {
-            func_set_longs.movel(reg_D1, addr_postinc_(reg_A1));
-        }
-        func_set_longs.dbra(reg_D0, "loop");
-    }
-    func_set_longs.rts();
+        func.lea(0xFFFC5A, reg_A1);
 
-    return rom.inject_code(func_set_longs);
+        // Clear heightmap with 0x4000
+        func.movel(0x40004000, reg_D1);
+        func.movel(reg_D1, reg_D2);
+        func.movel(reg_D1, reg_D3);
+        func.movel(reg_D1, reg_D4);
+        func.movel(reg_D1, reg_D5);
+        func.movel(reg_D1, reg_D6);
+        func.movel(reg_D1, reg_D7);
+        func.movel(reg_D1, reg_A0);
+        func.movel(342-1, reg_D0);
+        func.label("loop_hm");
+        {
+            func.movem({ reg_D1, reg_D2, reg_D3, reg_D4, reg_D5, reg_D6, reg_D7 }, { reg_A0 }, true, reg_A1, md::Size::LONG);
+        }
+        func.dbra(reg_D0, "loop_hm");
+        for(size_t i=0 ; i<2 ; ++i)
+            func.movel(reg_D1, addr_predec_(reg_A1));
+
+        // Clear foreground & background with 0x0000
+        func.clrl(reg_D1);
+        func.movel(reg_D1, reg_D2);
+        func.movel(reg_D1, reg_D3);
+        func.movel(reg_D1, reg_D4);
+        func.movel(reg_D1, reg_D5);
+        func.movel(reg_D1, reg_D6);
+        func.movel(reg_D1, reg_D7);
+        func.movel(reg_D1, reg_A0);
+        func.movel(684-1, reg_D0);
+        func.label("loop_tiles");
+        {
+            func.movem({ reg_D1, reg_D2, reg_D3, reg_D4, reg_D5, reg_D6, reg_D7 }, { reg_A0 }, true, reg_A1, md::Size::LONG);
+        }
+        func.dbra(reg_D0, "loop_tiles");
+        for(size_t i=0 ; i<4 ; ++i)
+            func.movel(reg_D1, addr_predec_(reg_A1));
+
+        func.clrl(reg_D0);
+    }
+    func.rts();
+
+    return rom.inject_code(func);
 }
 
 /**
@@ -42,117 +76,113 @@ static uint32_t inject_func_set_longs(md::ROM& rom)
  */
 static uint32_t inject_func_load_data_block(md::ROM& rom)
 {
-    md::Code func_load_data_block;
-    func_load_data_block.movem_to_stack({ reg_D0_D7 }, { reg_A3 });
-    func_load_data_block.clrl(reg_D6);
-    func_load_data_block.clrl(reg_D5);
-    func_load_data_block.clrl(reg_D4);
+    md::Code func;
+    func.movem_to_stack({ reg_D0_D7 }, { reg_A3 });
 
-    func_load_data_block.adda(reg_D1, reg_A1); // advance to first non-empty line
-    func_load_data_block.label("line");
+    func.adda(reg_D1, reg_A1); // advance to first non-empty line
+    func.label("line");
     {
         // Store line starting address (A1) inside A3
-        func_load_data_block.movel(reg_A1, reg_A3);
+        func.movel(reg_A1, reg_A3);
         {
-            func_load_data_block.adda(reg_D2, reg_A1); // advance to first non-empty word in line
-            func_load_data_block.movew(reg_D3, reg_D0); // setup the amount of words to copy inside D0
+            func.adda(reg_D2, reg_A1); // advance to first non-empty word in line
+            func.movew(reg_D3, reg_D0); // setup the amount of words to copy inside D0
 
             // copy D0 words from A0 to A1, or until a 0xFFFF is found
-            func_load_data_block.label("loop");
+            func.label("loop");
             {
                 // If there are still words to skip, skip it!
-                func_load_data_block.tstw(reg_D5);
-                func_load_data_block.bne("skip");
-                func_load_data_block.tstw(reg_D4);
-                func_load_data_block.bne("repeat");
+                func.tstw(reg_D5);
+                func.bne("skip");
+                func.tstw(reg_D4);
+                func.bne("repeat");
 
-                func_load_data_block.movew(addr_postinc_(reg_A0), reg_D7);
-                func_load_data_block.bmi("minus_case");
-                func_load_data_block.movew(reg_D7, addr_postinc_(reg_A1));
+                func.movew(addr_postinc_(reg_A0), reg_D7);
+                func.bmi("minus_case");
+                func.movew(reg_D7, addr_postinc_(reg_A1));
             }
-            func_load_data_block.label("next_loop_iteration");
-            func_load_data_block.dbra(reg_D0, "loop");
+            func.label("next_loop_iteration");
+            func.dbra(reg_D0, "loop");
         }
-        func_load_data_block.movel(reg_A3, reg_A1);
-        func_load_data_block.adda(LINE_SIZE_IN_BYTES, reg_A1); // advance to next line start
+        func.movel(reg_A3, reg_A1);
+        func.adda(LINE_SIZE_IN_BYTES, reg_A1); // advance to next line start
     }
-    func_load_data_block.bra("line");
+    func.bra("line");
 
-    func_load_data_block.label("ret");
-    func_load_data_block.movem_from_stack({ reg_D0_D7 }, { reg_A3 });
-    func_load_data_block.rts();
+    func.label("ret");
+    func.movem_from_stack({ reg_D0_D7 }, { reg_A3 });
+    func.rts();
 
     // Code block handling the case where we need to skip iterations
-    func_load_data_block.label("skip");
-    func_load_data_block.subiw(1, reg_D5);
-    func_load_data_block.addql(2, reg_A1);
-    func_load_data_block.bra("next_loop_iteration");
+    func.label("skip");
+    func.subiw(1, reg_D5);
+    func.addql(2, reg_A1);
+    func.bra("next_loop_iteration");
 
     // Code block handling the case where we need to repeat data
-    func_load_data_block.label("repeat");
-    func_load_data_block.subiw(1, reg_D4);
-    func_load_data_block.movew(reg_D6, addr_postinc_(reg_A1));
-    func_load_data_block.bra("next_loop_iteration");
+    func.label("repeat");
+    func.subiw(1, reg_D4);
+    func.movew(reg_D6, addr_postinc_(reg_A1));
+    func.bra("next_loop_iteration");
 
     // Code block handling the case where the msb is set
-    func_load_data_block.label("minus_case");
+    func.label("minus_case");
     // "10000000 00000000" case: block ending
-    func_load_data_block.andiw(0x7FFF, reg_D7);
-    func_load_data_block.beq("ret");
+    func.andiw(0x7FFF, reg_D7);
+    func.beq("ret");
 
     // Split the operand (D7) from the data (D6)
-    func_load_data_block.movew(reg_D7, reg_D6);
-    func_load_data_block.andiw(0x0FFF, reg_D6);
-    func_load_data_block.lsrw(8, reg_D7);
-    func_load_data_block.lsrw(4, reg_D7);
-    func_load_data_block.bne("not_skip");
+    func.movew(reg_D7, reg_D6);
+    func.andiw(0x0FFF, reg_D6);
+    func.lsrw(8, reg_D7);
+    func.lsrw(4, reg_D7);
+    func.bne("not_skip");
     {
         // "1000AAAA AAAAAAAA" case: skip A words
-        func_load_data_block.movew(reg_D6, reg_D5);  // Setup the amount of words to skip
-        func_load_data_block.bra("skip");            // Start the skipping loop
+        func.movew(reg_D6, reg_D5);  // Setup the amount of words to skip
+        func.bra("skip");            // Start the skipping loop
     }
-    func_load_data_block.label("not_skip");
-    func_load_data_block.cmpib(0x2, reg_D7);
-    func_load_data_block.bne("not_repeat_byte");
+    func.label("not_skip");
+    func.cmpib(0x2, reg_D7);
+    func.bne("not_repeat_byte");
     {
         // "1010AAAA BBBBBBBB" case: repeat A times byte B
         // Setup amount of repeats (D4)
-        func_load_data_block.movew(reg_D6, reg_D4);
-        func_load_data_block.lsrw(8, reg_D4);
+        func.movew(reg_D6, reg_D4);
+        func.lsrw(8, reg_D4);
         // Setup word to repeat (D6)
-        func_load_data_block.andiw(0x00FF, reg_D6);
-        func_load_data_block.bra("repeat");            // Start the repeat loop
+        func.andiw(0x00FF, reg_D6);
+        func.bra("repeat");            // Start the repeat loop
     }
-    func_load_data_block.label("not_repeat_byte");
-    func_load_data_block.cmpib(0x3, reg_D7);
-    func_load_data_block.bne("not_repeat_word");
+    func.label("not_repeat_byte");
+    func.cmpib(0x3, reg_D7);
+    func.bne("not_repeat_word");
     {
         // "1011AAAA AAAAAAAA" case: repeat next word A times
         // Setup amount of repeats (D4)
-        func_load_data_block.movew(reg_D6, reg_D4);
+        func.movew(reg_D6, reg_D4);
         // Setup word to repeat (D6)
-        func_load_data_block.movew(addr_postinc_(reg_A0), reg_D6);
-        func_load_data_block.bra("repeat");            // Start the repeat loop
+        func.movew(addr_postinc_(reg_A0), reg_D6);
+        func.bra("repeat");            // Start the repeat loop
     }
-    func_load_data_block.label("not_repeat_word");
-    func_load_data_block.cmpib(0x1, reg_D7);
-    func_load_data_block.bne("not_packed_bytes");
+    func.label("not_repeat_word");
+    func.cmpib(0x1, reg_D7);
+    func.bne("not_packed_bytes");
     {
         // "1001AAAA AABBBBBB" case: place byte A then byte B as words
-        func_load_data_block.movew(reg_D6, reg_D7);
-        func_load_data_block.lsrw(6, reg_D6);
-        func_load_data_block.movew(reg_D6, addr_postinc_(reg_A1));
+        func.movew(reg_D6, reg_D7);
+        func.lsrw(6, reg_D6);
+        func.movew(reg_D6, addr_postinc_(reg_A1));
         // Setup a 1 occurence repeat to make the whole process handle line changes correctly
-        func_load_data_block.movew(1, reg_D4); // 1 repeat
-        func_load_data_block.movew(reg_D7, reg_D6);
-        func_load_data_block.andiw(0x003F, reg_D6);
-        func_load_data_block.bra("next_loop_iteration");
+        func.movew(1, reg_D4); // 1 repeat
+        func.movew(reg_D7, reg_D6);
+        func.andiw(0x003F, reg_D6);
+        func.bra("next_loop_iteration");
     }
-    func_load_data_block.label("not_packed_bytes");
+    func.label("not_packed_bytes");
+    func.rts();
 
-
-
-    return rom.inject_code(func_load_data_block);
+    return rom.inject_code(func);
 }
 
 /**
@@ -163,40 +193,23 @@ static uint32_t inject_func_load_data_block(md::ROM& rom)
  * @param func_set_longs
  * @return
  */
-static uint32_t inject_func_load_map(md::ROM& rom, uint32_t func_load_data_block, uint32_t func_set_longs)
+static uint32_t inject_func_load_map(md::ROM& rom, uint32_t func_load_data_block, uint32_t func_clear_map_data)
 {
     md::Code func_load_map;
     func_load_map.movem_to_stack({ reg_D0_D7 }, { reg_A0, reg_A1 });
     {
-        func_load_map.clrl(reg_D0);
-
-        // Clear foreground & background with 0x0000
-        // FIXME: Perf --> 5476 MOVEL + 5476 DBRA
-        func_load_map.movel(0x00000000, reg_D1);
-        func_load_map.lea(0xFF7C02, reg_A1);
-        func_load_map.movew(5476, reg_D0); // 5476 longs
-        func_load_map.jsr(func_set_longs);
-
-        // Clear heightmap with 0x4000
-        // FIXME: Perf --> 2738 MOVEL + 2738 DBRA
-        func_load_map.movel(0x40004000, reg_D1);
-        func_load_map.lea(0xFFD192, reg_A1);
-        func_load_map.movew(5476 / 2, reg_D0); // 2738 longs
-        func_load_map.jsr(func_set_longs);
+        func_load_map.jsr(func_clear_map_data);
 
         // --------------------------------------------------------------------------------------------------------
 
         // Read metadata for tiles
-        func_load_map.clrl(reg_D1);
         func_load_map.moveb(addr_(reg_A2), reg_D1); // = layout->top() = 9 = amount of big chunks to skip
         func_load_map.lslw(1, reg_D1);
         func_load_map.mulu(bval_(LINE_SIZE_IN_WORDS), reg_D1); // D1 = offset from block start to reach first non-empty line
 
-        func_load_map.clrl(reg_D2);
         func_load_map.moveb(addr_(reg_A2, 1), reg_D2); // = layout->left() = 8 = amount of small words to skip
         func_load_map.lslw(1, reg_D2); // D2 = offset to apply from chunk start to first non-empty data in chunk
 
-        func_load_map.clrl(reg_D3);
         func_load_map.moveb(addr_(reg_A2, 2), reg_D3); // = layout->width() = 14 = words to copy for each line (before going to next chunk)
         func_load_map.subib(1, reg_D3);
 
@@ -281,10 +294,10 @@ void new_map_format(md::ROM& rom, World& world)
 
     std::cout << "Full map data for all " << unique_map_layouts.size() << " layouts takes " << total_size/1000 << "KB" << std::endl;
 
-    uint32_t func_set_longs = inject_func_set_longs(rom);
+    uint32_t func_clear_map_data = inject_func_clear_map_data(rom);
     uint32_t func_load_data_block = inject_func_load_data_block(rom);
 
-    uint32_t func_load_map = inject_func_load_map(rom, func_load_data_block, func_set_longs);
+    uint32_t func_load_map = inject_func_load_map(rom, func_load_data_block, func_clear_map_data);
 
     std::cout << "func_load_map = 0x" << std::hex << func_load_map << std::dec << std::endl;
     std::cout << "func_load_data_block = 0x" << std::hex << func_load_data_block << std::dec << std::endl;
